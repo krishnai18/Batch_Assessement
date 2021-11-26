2c6522329e41f51eaf4bdb43381227bb4a3a00d7
krishnai18 <82368339+krishnai18@users.noreply.github.com>
2021-11-26 10:23:34 +0530

    Add files via upload

---
+++ BatchAssessment/Data/Migrations/20211124202828_addBatchtoDb.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BatchAssessment.Migrations
+{
+    public partial class addBatchtoDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Batches",
+                columns: table => new
+                {
+                    BatchId = table.Column<Guid>(nullable: false),
+                    BusinessUnit = table.Column<string>(nullable: false),
+                    ExpiryDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Batches", x => x.BatchId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ACLs",
+                columns: table => new
+                {
+                    ReadUser = table.Column<string>(nullable: false),
+                    ReadGroup = table.Column<string>(nullable: true),
+                    BatchModelBatchId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ACLs", x => x.ReadUser);
+                    table.ForeignKey(
+                        name: "FK_ACLs_Batches_BatchModelBatchId",
+                        column: x => x.BatchModelBatchId,
+                        principalTable: "Batches",
+                        principalColumn: "BatchId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Attributes",
+                columns: table => new
+                {
+                    Key = table.Column<string>(nullable: false),
+                    Value = table.Column<string>(nullable: false),
+                    BatchModelBatchId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attributes", x => x.Key);
+                    table.ForeignKey(
+                        name: "FK_Attributes_Batches_BatchModelBatchId",
+                        column: x => x.BatchModelBatchId,
+                        principalTable: "Batches",
+                        principalColumn: "BatchId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ACLs_BatchModelBatchId",
+                table: "ACLs",
+                column: "BatchModelBatchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attributes_BatchModelBatchId",
+                table: "Attributes",
+                column: "BatchModelBatchId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ACLs");
+
+            migrationBuilder.DropTable(
+                name: "Attributes");
+
+            migrationBuilder.DropTable(
+                name: "Batches");
+        }
+    }
+}
